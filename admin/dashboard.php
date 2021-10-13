475daceaa8961d97df4d678bb994e69afad9b1a4
WELINGTON DE OLIVEIRA PINCER <21193432@CFP127ALUNO.LOCAL>
2021-10-13 16:55:25 -0300

    Planejamento do dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title> Dashboard </title>
+    </head>
+    <body>
+        <header>
+            <div>
+            
+            </div>
+        </header>
+        <main>
+        
+        </main>
+        <footer>
+        
+        </footer>
+    </body>
+</html>
