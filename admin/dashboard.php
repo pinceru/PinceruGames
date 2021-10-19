04210297fe831231b5e5b7107e09274d16c0e58c
welington <welington.pincer@gmail.com>
2021-10-18 23:34:47 -0300

    Dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,11 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title> Dashboard </title>
+    <link href="css/style.css" type="text/css" rel="stylesheet">
     </head>
     <body>
         <header>
-            <div>
-            
+            <div id="conteudoCabecalho">
+                <div id="conteudoEsquerdo">
+                    <div id="linhaSuperior">
+                        <div id="cms">
+                            <label> C M S </label>
+                        </div>
+                        <div id="titulo">
+                            <label> PINCERU GAMES </label>
+                        </div>
+                    </div>
+                    <div id="linhaInferior">
+                        <label> Gerenciamento do conteúdo do site</label>
+                    </div>
+                </div>
+                <div id="conteudoDireito">
+                    <img src="../img/logoMarca.PNG" id="logo" title="Logo Pinceru Games">
+                </div>
             </div>
         </header>
         <main>
